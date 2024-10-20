c4e909ff1a6e9cf4b469793a4d0fa3800f7c5bd1
mayyamayjai00 <yanisapearsri2548@gmail.com>
2024-10-20 22:58:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-# YOUR CODE HERE
+n = int(input())
+def summation(a,b,c) :
+  for i in range(n) :
+    sum = a[i] + b[i]
+    c.append(sum)
+  return sum
+def find_min_max(a) :
+  z.sort()
+  print(f"({a[0]}, {a[n-1]})")
+  return a
+list1 = []
+list2 = []
+update = []
+for i in range(2*n) :
+  num = int(input())
+  if i < n :
+    list1.append(num)
+  else :
+    list2.append(num)
+
+summation(list1,list2,update)
+print(update)
+find_min_max(update)
